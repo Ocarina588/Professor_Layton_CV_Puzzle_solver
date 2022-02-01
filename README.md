222e6b1fd730c9bb7f5813b9395db9850032cb9d
manuelito <manuel.medina-marais@epitech.eu>
2022-02-01 19:12:16 +0100

    [~] updating readme

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 <p align="center">
-  <img src="assets/professor-layton-curious-village.png" />
+  <img align="center" src="assets/professor-layton-curious-village.png" />
 </p>
 
-# Professor Layton and the Curious Village -- Crouton's Pitchers
+<h1 align="center"> Professor Layton and the Curious Village -- Crouton's Pitchers </h1>
+
+This project was made to find the best solution in a series of puzzle in the game "Professor Layton and the Curious Village"
+
+# Puzzles
+
+- 023 Juice Pitchers
+- 024 Milk Pitchers
+- 078 Water Pitchers
+
+<p align="center">
+  <img align="center" width="300"src="assets/IMG-4054.jpg" />
+  <img align="center" width="300"src="assets/IMG-4055.jpg" />
+  <img align="center" width="300"src="assets/IMG-4056.jpg" />
+</p>
--- main.cpp
+++ main.cpp
@@ -7,21 +7,13 @@ typedef union {
     char pitchers[3];
 } state_u;
 
-std::pair<int, int> get_variables(void)
+std::pair<int, int> get_variables(int ac, char **av)
 {
-    int limits[3];
-    int goal[3];
+    if (ac != 7)
+        throw std::runtime_error("bad arguments");
 
-    std::cout << "what are the capacity of each pitcher ? [ex: 16 9 7]" << std::endl;
-    std::cout << ": ";
-    std::cin >> limits[0] >> limits[1] >> limits[2];
-    std::cout << "what are the goal of each pitcher ? [ex: 8 8 0]" << std::endl;
-    std::cout << ": ";
-    std::cin >> goal[0] >> goal[1] >> goal[2];
-    std::cout << std::endl;
-
-    state_u x = {.pitchers = {(char)limits[0], (char)limits[1], (char)limits[2]}};
-    state_u y = {.pitchers = {(char)goal[0], (char)goal[1], (char)goal[2]}};
+    state_u x = {.pitchers = {(char)std::atoi(av[1]), (char)std::atoi(av[2]), (char)std::atoi(av[3])}};
+    state_u y = {.pitchers = {(char)std::atoi(av[4]), (char)std::atoi(av[5]), (char)std::atoi(av[6])}};
 
     return std::make_pair(x.id, y.id);
 }
@@ -43,46 +35,52 @@ bool check_victory(std::pair<int, std::string> state, state_u goal)
     return true;
 }
 
+bool algorithm(int from, int to, state_u limits, state_u goal, std::queue<std::pair<int, std::string>> &states,
+    std::unordered_set<int> &seen_states, std::pair<int, std::string> &state)
+{
+
+    auto new_state = state;
+    state_u s = {state.first};
+
+    if (!is_move_good(from, to, s, limits)) return (false);
+
+    int max = limits.pitchers[to] - s.pitchers[to];
+    int r = max - s.pitchers[from] < 0 ? max : s.pitchers[from];
+
+    s.pitchers[from] -= r;
+    s.pitchers[to] += r;
+
+    if (seen_states.find(s.id) != seen_states.end()) return (false);
+    seen_states.insert(s.id);
+
+    new_state.first = s.id;
+    new_state.second += std::to_string(from) + " > " + std::to_string(to) + "\n";
+
+    if (check_victory(new_state, goal))
+        return (true);
+
+    states.push(new_state);
+    return (false);
+}
+
 int main(int ac, char **av)
 {
-    auto variables = get_variables();
+    std::pair<int, int> variables = get_variables(ac, av);
     state_u limits = {variables.first};
     state_u goal = {variables.second};
-    state_u initial_state = {.pitchers = {limits.pitchers[0]}};
 
     std::unordered_set<int> seen_states;
     std::queue<std::pair<int, std::string>> states;
 
-    states.push({initial_state.id, ""});
+    states.push({limits.pitchers[0], ""});
 
     while (!states.empty()) {
         auto state = states.front(); states.pop();
 
         for (int from = 0; from < 3 ; from++)
-            for (int to = 0; to < 3 ; to++) {
-
-                auto new_state = state;
-                state_u s = {state.first};
-
-                if (!is_move_good(from, to, s, limits)) continue;
-
-                int max = limits.pitchers[to] - s.pitchers[to];
-                int r = max - s.pitchers[from] < 0 ? max : s.pitchers[from];
-
-                s.pitchers[from] -= r;
-                s.pitchers[to] += r;
-
-                if (seen_states.find(s.id) != seen_states.end()) continue;
-                seen_states.insert(s.id);
-
-                new_state.first = s.id;
-                new_state.second += std::to_string(from) + " > " + std::to_string(to) + "\n";
-
-                if (check_victory(new_state, goal))
+            for (int to = 0; to < 3 ; to++)
+                if (algorithm(from, to, limits, goal, states, seen_states, state))
                     return (0);
-
-                states.push(new_state);
-            }
     }
 
     std::cout << "There's not a solution for this puzzle :(" << std::endl;
